dabe3ae57c2918a940f0ae8449f3aa850aea9d88
Ricardo Parra molina <rparra@MacBook-Pro-de-Ricardo.local>
2019-12-29 12:59:14 -0300

    feat: menu config

---
+++ app/Http/Controllers/MailingController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MailingController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('mailing.productors');
+    }
+}
--- resources/views/mailing/productors.blade.php
+++ resources/views/mailing/productors.blade.php
@@ -1,4 +1,3 @@
-
 @extends('adminlte::page')
 
 @section('title', 'Dashboard')
@@ -8,7 +7,7 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    @include('mailing.body')
 @stop
 
 @section('css')
